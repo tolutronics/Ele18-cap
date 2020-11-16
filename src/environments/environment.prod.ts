9c4b14fd91c196bd86c52dac05c9f89515cd7d70
tolutronics <toluxy100@gmail.com>
2020-11-16 10:27:56 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
 export const environment = {
   production: true,
     firebase: {
-      apiKey: 'AIzaSyCCewB9tgK80XxI-AVybHRf5jWv_22Y_pM',
-    authDomain: 'ele18-e8860.firebaseapp.com',
-    databaseURL: 'https://ele18-e8860.firebaseio.com',
-    projectId: 'ele18-e8860',
-    storageBucket: 'ele18-e8860.appspot.com',
-    messagingSenderId: '1035414255055',
-    appId: '1:1035414255055:web:b3d01b734c370c20'
+      
   }
 };
